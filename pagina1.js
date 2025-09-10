e02cbc3f5d64a3ceb421e11949eeef9063e23a0f
MRemi013 <luisemilianodiaz929@gmail.com>
2025-09-10 17:05:13 -0300

    casi terminado

--- pagina1.js
+++ pagina1.js
@@ -8,4 +8,14 @@
 
 
 //footer
+const footer = document.getElementById('footer');
+footer.classList.add('bg-dark');
+footer.classList.add('text-white');
+footer.classList.add('text-center');    
 
+
+const listaLinks=document.querySelectorAll(".nav-link");
+listaLinks.forEach(element => {
+    element.classList.add("text-white");
+    console.log(element);
+});
