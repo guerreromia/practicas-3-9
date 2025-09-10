cc8bc2c03b30490d39de33ada59825bd60448271
ariiialegria <arialegriahs1@gmail.com>
2025-09-10 16:58:00 -0300

    casi terminado

--- pagina1.js
+++ pagina1.js
@@ -6,221 +6,147 @@
 //miranda
 //contenedor
 
-let botonAdd= document.createElement("button")
-botonAdd.classList.add("btn")
-botonAdd.classList.add("btn-primary")
-botonAdd.classList.add("mx-1")
-botonAdd.textContent="Comprar"
+class Tarjeta {
+    constructor(titulo, imagen, descripcion, precio) {
+        this.titulo = titulo;
+        this.imagen = imagen;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+    agregarAlbum(album) {
 
+    }
+
+}
 let contenedor = document.getElementById("contenedor")
+contenedor.classList.add("conteiner")
 contenedor.style.margin = "25px"
-contenedor.style.display="flex"
+contenedor.style.display = "flex"
 
-//cartas
-let card1 = document.createElement("div")
-card1.classList.add("card")
-card1.style.width = "300px"
-card1.style.margin="1rem"
+let row = document.createElement("div")
+row.classList.add("row")
+let albums = [
+    new Tarjeta("El Disco De Tu Corazón", "https://i.scdn.co/image/ab67616d0000b2735195219d777c551f280d95d5", "Miranda!\nFormato: CD MUSICAL", 25000),
+    new Tarjeta("Don't Tap the Glass", "https://upload.wikimedia.org/wikipedia/en/b/bb/Tyler%2C_the_Creator_-_Don%27t_Tap_the_Glass.png", "Tyler The Creator \nFormato: CD MUSICAL", 35000),
+    new Tarjeta("Por Cesárea", "https://images.genius.com/5bdb578d1dad91f670071fa9c30603ed.1000x1000x1.png", "Dillom \nFormato: Vinilo", 40000),
+    new Tarjeta("DAMN.", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb5WDlRTHe1EdYmifyyyTqTmMaSsVdd9EVvQ&s", "Kendrick Lamar \nFormato: CD MUSICAL", 20000),
+    new Tarjeta("The Dark Side of the Moon", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKqByhEFZaHf3OoW7oYwdsUQxjSbxn6SOquQ&s", " Pink Floyd \nFormato: Vinilo", 100000),
+    new Tarjeta("Ballads 1", "https://i.scdn.co/image/ab67616d0000b27360ba1d6104d0475c7555a6b2", "Joji \nFormato: CD MUSICAL", 25000),
+    new Tarjeta("Karma", "https://akamai.sscdn.co/letras/360x360/albuns/3/6/8/0/3792071755868469.jpg", "Stray Kids\n Formato: ALBUM", 60000),
+    new Tarjeta("The Tortured Poets Department", "https://upload.wikimedia.org/wikipedia/en/6/6e/Taylor_Swift_%E2%80%93_The_Tortured_Poets_Department_%28album_cover%29.png", "Taylor Swift \nFormato:Vinilo", 40000),
+    new Tarjeta("NO VAYAS A ATENDER CUANDO EL DEMONIO LLAMA", "https://i.scdn.co/image/ab67616d0000b27306ad26d22030b8c988a10f20", "LALI \nFormato: CD MUSICAL", 25000),
+    new Tarjeta("Scarlet", "https://upload.wikimedia.org/wikipedia/en/0/0b/Doja_Cat_-_Scarlet.png", "Doja Cat \nFormato: CD MUSICAL", 25000),
+    new Tarjeta("Oktubre", "https://akamai.sscdn.co/uploadfile/letras/albuns/6/3/e/4/2183691712605514.jpg", " Patricio Rey y sus Redonditos de Ricota \nFormato:CD MUSICAL", 30000),
+    new Tarjeta("Baño María", "https://i.scdn.co/image/ab67616d0000b27340349e09eb8cc8cac72f7959", " CA7RIEL & Paco Amoroso \nFormato: CD MUSICAL", 30000)
+];
+function MostrarAlbumes() {
+    albums.forEach(x => {
+        const col = document.createElement("div")
+        col.classList.add("col-md-6");
+        col.classList.add("col-lg-4")
 
-// //imagen del album
-let imagenMiranda = document.createElement("img")
-imagenMiranda.src =("https://i.scdn.co/image/ab67616d0000b2735195219d777c551f280d95d5")
-imagenMiranda.classList.add("card-img-top")
-imagenMiranda.alt = "imagen del album de miranda!"
-card1.appendChild(imagenMiranda)
+        //tarjeta
+        let card = document.createElement("div")
+        card.classList.add("card")
+        card.style.width = "250px"
+        card.style.margin = "1rem"
 
-// //cuerpo
-let cardBody1 = document.createElement("div")
-cardBody1.classList.add("card-body")
-card1.appendChild(cardBody1)
+        //imageb
+        const imagen = document.createElement("img")
+        imagen.classList.add("card-img-top")
+        imagen.src = x.imagen
+        card.appendChild(imagen)
 
-//titulo del album
-let tituloMiranda = document.createElement("h5")
-tituloMiranda.textContent = "El Disco De Tu Corazón"
-cardBody1.appendChild(tituloMiranda)
+        //cuerpo
+        const cardBody = document.createElement("div")
+        cardBody.classList.add("card-body")
 
-//texto del album
-let descripcionMiranda = document.createElement("p")
-descripcionMiranda.classList.add("parrafo")
-descripcionMiranda.textContent = " Miranda!\nFormato: CD MUSICAL \nPrecio: 25.000$"
-cardBody1.appendChild(descripcionMiranda)
-cardBody1.appendChild(botonAdd)
-card1.appendChild(cardBody1)
+        //titulo
+        const titulo = document.createElement("h5")
+        titulo.classList.add("card-tittle")
+        titulo.textContent = `${x.titulo}`
+        cardBody.appendChild(titulo)
 
+        //descripcion
+        const parrafo = document.createElement("p")
+        parrafo.classList.add("card-text")
+        parrafo.textContent = `${x.descripcion}\n precio: $${x.precio}`
 
-contenedor.appendChild(card1)
+        //boton de comprar
+        let botonAdd = document.createElement("button")
+        botonAdd.classList.add("btn")
+        botonAdd.classList.add("btn-primary")
+        botonAdd.classList.add("mx-1")
+        botonAdd.textContent = "Comprar"
 
-//tyler
-//cartas
-let card2 = document.createElement("div")
-card2.classList.add("card")
-card2.style.width = "300px"
-card2.style.margin="1rem"
+        botonAdd.addEventListener("click", function () {
+            persona1.agregarProducto(x)
+            Mostrar()
+        })
+        cardBody.append(parrafo, botonAdd)
+        card.appendChild(cardBody)
+        col.appendChild(card)
+        row.appendChild(col)
 
-// //imagen del album
-let imagenTyler = document.createElement("img")
-imagenTyler.src =("https://upload.wikimedia.org/wikipedia/en/b/bb/Tyler%2C_the_Creator_-_Don%27t_Tap_the_Glass.png")
-imagenTyler.classList.add("card-img-top")
-imagenTyler.alt = "imagen del album de tyler"
-card2.appendChild(imagenTyler)
+    })
 
-// //cuerpo
-let cardBody2 = document.createElement("div")
-cardBody2.classList.add("card-body")
-card2.appendChild(cardBody2)
+}
+contenedor.appendChild(row)
+MostrarAlbumes()
 
-//titulo del album
-let tituloTyler = document.createElement("h5")
-tituloTyler.textContent = "Don't Tap the Glass"
-cardBody2.appendChild(tituloTyler)
+class Persona {
+    constructor(nombre, apellido, dni, telefono, email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.email = email;
+        this.productos = [];
+    }
+    datosCompletos() {
+        return `Nombre:${this.nombre}, Apellido:${this.apellido}, DNI:${this.dni}, Telefono:${this.telefono}, Correo Electronico:${this.email}`
+    }
+    agregarProducto(producto) {
+        this.productos.push(producto)
+    }
+    subTotal() {
+        let total = 0;
+        this.productos.forEach(x => total += x.precio)
+        return total
+    }
+}
+const persona1 = new Persona("Carlos", "Millahueque", 48866750, "299-5242501", "charly_maniac83@gmail.com")
 
-//texto del album
-let descripcionTyler = document.createElement("p")
-descripcionTyler.classList.add("parrafo")
-descripcionTyler.textContent = " Tyler The Creator \nFormato: CD MUSICAL \nPrecio: 35.000$"
-cardBody2.appendChild(descripcionTyler)
-cardBody2.appendChild(botonAdd)
-card2.appendChild(cardBody2)
+let tbody = document.querySelector("#tbody")
+let subtotal=document.querySelector("#subtotal")
+//carrito de comprasss
+function Mostrar() {
+     if (subtotal.innerHTML !== 0) { //resetear 0 el total
+            subtotal.textContent = 0
+        }
+        if (tbody.innerHTML !== "") {
+            tbody.innerHTML = ""
+        }
+    persona1.productos.forEach(x => {
+        //carrito
+        const tr = document.createElement("tr")
+        const td1 = document.createElement("td")
+        td1.textContent = x.titulo
+        const td2 = document.createElement("td")
+        td2.textContent = x.descripcion
+        const td3 = document.createElement("td")
+        td3.textContent = x.precio
+        const td4 = document.createElement("td")
 
+        //total
+        subtotal.textContent = persona1.subTotal()
+        console.log(persona1.subTotal())
 
-contenedor.appendChild(card2)
-
-//dillom
-//cartas
-let card3 = document.createElement("div")
-card3.classList.add("card")
-card3.style.width = "300px"
-card3.style.margin="1rem"
-
-// //imagen del album
-let imagenDillom = document.createElement("img")
-imagenDillom.src =("https://images.genius.com/5bdb578d1dad91f670071fa9c30603ed.1000x1000x1.png")
-imagenDillom.classList.add("card-img-top")
-imagenDillom.alt = "dillom album"
-card3.appendChild(imagenDillom)
-
-// //cuerpo
-let cardBody3 = document.createElement("div")
-cardBody3.classList.add("card-body")
-card3.appendChild(cardBody3)
-
-//titulo del album
-let tituloDillom = document.createElement("h5")
-tituloDillom.textContent = "Por Cesárea"
-cardBody3.appendChild(tituloDillom)
-
-//texto del album
-let descripcionDillom = document.createElement("p")
-descripcionDillom.classList.add("parrafo")
-descripcionDillom.textContent = " Dillom \nFormato: Vinilo \nPrecio: 40.000$"
-cardBody3.appendChild(descripcionDillom)
-cardBody3.appendChild(botonAdd)
-card3.appendChild(cardBody3)
-
-
-contenedor.appendChild(card3)
-
-//kendrick
-let card4 = document.createElement("div")
-card4.classList.add("card")
-card4.style.width = "300px"
-card4.style.margin="1rem"
-
-// //imagen del album
-let imagenKendrick = document.createElement("img")
-imagenKendrick.src =("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb5WDlRTHe1EdYmifyyyTqTmMaSsVdd9EVvQ&s")
-imagenKendrick.classList.add("card-img-top")
-imagenKendrick.alt = "kendrick album"
-card4.appendChild(imagenKendrick)
-
-// //cuerpo
-let cardBody4 = document.createElement("div")
-cardBody4.classList.add("card-body")
-card4.appendChild(cardBody4)
-
-//titulo del album
-let tituloKendrick = document.createElement("h5")
-tituloKendrick.textContent = "DAMN."
-cardBody4.appendChild(tituloKendrick)
-
-//texto del album
-let descripcionKendrick= document.createElement("p")
-descripcionKendrick.classList.add("parrafo")
-descripcionKendrick.textContent = " Kendrick Lamar \nFormato: CD MUSICAL \nPrecio: 20.000$"
-cardBody4.appendChild(descripcionKendrick)
-cardBody4.appendChild(botonAdd)
-card4.appendChild(cardBody4)
+        tr.append(td1, td2, td3)
+        tbody.appendChild(tr)
 
 
-contenedor.appendChild(card4)
-
-// pink floyd
-let card5 = document.createElement("div")
-card5.classList.add("card")
-card5.style.width = "300px"
-card5.style.margin="1rem"
-
-// //imagen del album
-let imagenPink = document.createElement("img")
-imagenPink.src =("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKqByhEFZaHf3OoW7oYwdsUQxjSbxn6SOquQ&s")
-imagenPink.classList.add("card-img-top")
-imagenPink.alt = "kendrick album"
-card5.appendChild(imagenPink)
-
-// //cuerpo
-let cardBody5 = document.createElement("div")
-cardBody5.classList.add("card-body")
-card5.appendChild(cardBody5)
-
-//titulo del album
-let tituloPink = document.createElement("h5")
-tituloPink.textContent = "The Dark Side of the Moon"
-cardBody5.appendChild(tituloPink)
-
-//texto del album
-let descripcionPink= document.createElement("p")
-descripcionPink.classList.add("parrafo")
-descripcionPink.textContent = " Pink Floyd \nFormato: Vinilo \nPrecio: 100.000$"
-cardBody5.appendChild(descripcionPink)
-cardBody5.appendChild(botonAdd)
-card5.appendChild(cardBody5)
+    })
+}
 
 
-contenedor.appendChild(card5)
-
-//joji
-let card6 = document.createElement("div")
-card6.classList.add("card")
-card6.style.width = "300px"
-card6.style.margin="1rem"
-
-// //imagen del album
-let imagenJoji = document.createElement("img")
-imagenJoji.src =("https://i.scdn.co/image/ab67616d0000b27360ba1d6104d0475c7555a6b2")
-imagenJoji.classList.add("card-img-top")
-imagenJoji.alt = "kendrick album"
-card6.appendChild(imagenJoji)
-
-// //cuerpo
-let cardBody6 = document.createElement("div")
-cardBody6.classList.add("card-body")
-card6.appendChild(cardBody6)
-
-//titulo del album
-let tituloJoji = document.createElement("h5")
-tituloJoji.textContent = "Ballads 1"
-cardBody6.appendChild(tituloJoji)
-
-//texto del album
-let descripcionJoji= document.createElement("p")
-descripcionJoji.classList.add("parrafo")
-descripcionJoji.textContent = " Joji \nFormato: CD MUSICAL\nPrecio: 25.000$"
-cardBody6.appendChild(descripcionJoji)
-cardBody6.appendChild(botonAdd)
-card6.appendChild(cardBody6)
-
-contenedor.appendChild(card6)
-
-/* botonAdd.addEventListener("click",function(){
-    persona1.agregarProducto(p)
-}) */
